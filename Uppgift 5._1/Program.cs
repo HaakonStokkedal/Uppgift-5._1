d825150c0145889abf29199315857d11dbb4c9d6
hast06001 <hast06001@E-PW024NW1>
2023-11-22 12:34:01 +0100

    Add project files.

---
+++ Uppgift 5._1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = new string[4];
+
+            meningar[0] = "Hej och välkommen!";
+            meningar[1] = "Detta program skriver ut fyra meningar.";
+            meningar[2] = "Alla meningar är på olika rader.";
+            meningar[3] = "De har även ett mellanrum mellan varje rad.";
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(meningar[i]);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
